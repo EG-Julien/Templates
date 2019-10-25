d8e659fcbfc8bca144fcc2e8c3fa8d1c9cf0f18e
Julien <iteamdev33@gmail.com>
2019-10-25 08:28:53 +0200

    Update config.php

--- config.php
+++ config.php
@@ -4,7 +4,7 @@
     CONFIG DATABASE
  */
 
-$dbuser     = "manifeste";
-$dbpassword = "Ji0EnouHj?~b";
-$dbhost     = "den1.mysql4.gear.host";
-$dbname     = "manifeste";
+$dbuser     = "root";
+$dbpassword = "root";
+$dbhost     = "localhost";
+$dbname     = "db";
